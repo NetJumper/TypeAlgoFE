a4d965ef19887c1322a117bed9263285357f9a59
Jose Iriarte <joseiriarte.r@gmail.com>
2024-03-27 16:08:41 -0700

    TypeAlgo redirects to main page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,30 +3,28 @@ import { Link } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
   return (
+    // Navbar fixed to the top of the page, full width, with styling and shadow effect
     <nav className="fixed top-0 left-0 w-full bg-gray-700 text-slate-100 z-10 shadow-md">
       <div className="max-w-7xl mx-auto px-5 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            
-            <div className="hidden md:flex items-center space-x-4">
-              
-              <Link to="/type" className="px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700 hover:text-blue-300">
+            {/* Navigation Links: adjusted to show the correct routing paths and fixed the visible navigation links */}
+            <div className="md:flex items-center space-x-4">
+              {/* Link to the TypePractice page */}
+              <Link to="/" className="px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700 hover:text-blue-300">
                 TypeAlgo
               </Link>
-              <Link to="/contact" className="px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700 hover:text-blue-300">
-                {/* Leaderboard */}
-              </Link>
-              <Link to="/donate" className="px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700 hover:text-blue-300">
-                {/* Donate */}
-              </Link>
+              {/* Future placeholders for other sections like Leaderboard and Donate, if needed */}
             </div>
           </div>
-          {/* Sign In Button on the right */}
+          {/* Auth Links: Sign In and Sign Up, aligned to the right */}
           <div>
+            {/* Link to SignInPage */}
             <Link to="/signin" className="px-3 py-2 bg-gray-950 text-white rounded-md text-sm font-medium hover:bg-blue-300 transition duration-300">
               Log in
             </Link>
-            <Link to="/signup" className="px-3 py-2 bg-gray-950 text-white rounded-md text-sm font-medium hover:bg-blue-300 transition duration-300">
+            {/* Link to SignUpPage */}
+            <Link to="/signup" className="ml-2 px-3 py-2 bg-gray-950 text-white rounded-md text-sm font-medium hover:bg-blue-300 transition duration-300">
               Sign Up
             </Link>
           </div>
